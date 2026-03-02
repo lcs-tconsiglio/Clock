fcfb3d5d515a787862972b13e87eeb5e3ee77323
Theresa Consiglio <tconsiglio@lcs.on.ca>
2026-03-02 00:54:38 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Theresa Consiglio on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Theresa Consiglio on 2026-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
