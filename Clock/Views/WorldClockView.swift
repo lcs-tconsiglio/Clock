e7c3abb7b06679eb4dd12785c725719dda17a6b0
Theresa Consiglio <tconsiglio@lcs.on.ca>
2026-03-05 01:27:57 -0500

    I added a subview for ottawa.

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -10,26 +10,43 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack{
-            Text("World Clock")
-                .navigationTitle("World Clock")
-                .toolbar {
-                    
-                    ToolbarItem(placement: .topBarLeading) {
-                        
-                        Button("Edit") {
-                            // Does nothing right now
-                        }
-                        
+            VStack {
+                
+                HStack {
+                    VStack {
+                        Text("Today, +0HRS")
+                        Text("Ottawa")
+                            .font(.system(.largeTitle, design: .default, weight: .thin))
                     }
-                    ToolbarItem(placement: .primaryAction) {
-                        Button {
-                        } label: {
-                            Image(systemName: "plus")
-                        }
-                        
-                    }
+                    Spacer()
                     
+                    Text("6:35")
+                        .font(.system(size: 64.0, weight: .thin,design: .default, ))
+                    Text("AM")
+                        .font(.system( .largeTitle, design: .default, weight: .thin ))
                 }
+                
+                
+                    .navigationTitle("World Clock")
+                    .toolbar {
+                        
+                        ToolbarItem(placement: .topBarLeading) {
+                            
+                            Button("Edit") {
+                                // Does nothing right now
+                            }
+                            
+                        }
+                        ToolbarItem(placement: .primaryAction) {
+                            Button {
+                            } label: {
+                                Image(systemName: "plus")
+                            }
+                            
+                        }
+                        
+                    }
+            }
         }
         }
 }
